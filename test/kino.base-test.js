66876f75307848ff433e6d72c77a16e614d85321
kinogam <kinogam@gmail.com>
2013-10-22 09:25:28 +0800

    remove kino.module support add require.js amd support and change observer model parameters

--- test/kino.base-test.js
+++ test/kino.base-test.js
@@ -107,105 +107,105 @@ test("can pass data to event handler", function () {
 
 });
 
-module("module and use");
+//module("module and use");
 
-test("can use module after define it", function () {
-    expect(1);
+//test("can use module after define it", function () {
+//    expect(1);
 
-    kino.module("MyModule", function (expects) {
-        expects.sayHello = function (name) { return "hello " + name;};
-    });
+//    kino.module("MyModule", function (expects) {
+//        expects.sayHello = function (name) { return "hello " + name;};
+//    });
 
-    kino.use(["MyModule"], function (myModule) {
-        equal(myModule.sayHello("kino"), "hello kino");
-    });
-});
+//    kino.use(["MyModule"], function (myModule) {
+//        equal(myModule.sayHello("kino"), "hello kino");
+//    });
+//});
 
-test("module can dependency on other module", function () {
-    kino.module("MyWord", function (exports) {
-        exports.str = "hello";
-    });
+//test("module can dependency on other module", function () {
+//    kino.module("MyWord", function (exports) {
+//        exports.str = "hello";
+//    });
 
-    kino.module("MyModule", ["MyWord"], function (myword, expects) {
-        expects.sayHello = function (name) { return myword.str +" " + name; };
-    });
+//    kino.module("MyModule", ["MyWord"], function (myword, expects) {
+//        expects.sayHello = function (name) { return myword.str +" " + name; };
+//    });
 
-    kino.use(["MyModule"], function (myModule) {
-        equal(myModule.sayHello("kino"), "hello kino");
-    });
+//    kino.use(["MyModule"], function (myModule) {
+//        equal(myModule.sayHello("kino"), "hello kino");
+//    });
 
-});
+//});
 
-test("can define a module by json object", function () {
-    kino.module("model", {
-        str: 'hello'
-    });
+//test("can define a module by json object", function () {
+//    kino.module("model", {
+//        str: 'hello'
+//    });
 
-    kino.use(["model"], function (model) {
-        equal(model.str, 'hello');
-    });
+//    kino.use(["model"], function (model) {
+//        equal(model.str, 'hello');
+//    });
 
-});
+//});
 
-test("can use export to inject", function () {
-    expect(2);
+//test("can use export to inject", function () {
+//    expect(2);
 
-    kino.module("IService", {
-        asynGetFlightData: function (callback) {
-        }
-    });
+//    kino.module("IService", {
+//        asynGetFlightData: function (callback) {
+//        }
+//    });
 
-    kino.module("FlightController", ["IService"], function (IService, exports) {
-        kino.extend(exports, kino.Events);
+//    kino.module("FlightController", ["IService"], function (IService, exports) {
+//        kino.extend(exports, kino.Events);
 
-        exports.dosomething = function () {
-            IService.asynGetFlightData(function (result) {
-                exports.trigger("flightDataLoaded", [result]);
-            });
-        }
-    });
+//        exports.dosomething = function () {
+//            IService.asynGetFlightData(function (result) {
+//                exports.trigger("flightDataLoaded", [result]);
+//            });
+//        }
+//    });
 
-    kino.export("IService", {
-        asynGetFlightData: function (callback) {
-            callback([{ id: '123' }, { id: '234' }]);
-        }
-    });
+//    kino.export("IService", {
+//        asynGetFlightData: function (callback) {
+//            callback([{ id: '123' }, { id: '234' }]);
+//        }
+//    });
 
-    kino.use(["FlightController"], function (controller) {
-        controller.on("flightDataLoaded", function (e, result) {
-            equal(result[0].id, '123');
-            equal(result[1].id, '234');
-        });
-        controller.dosomething();
-    });
+//    kino.use(["FlightController"], function (controller) {
+//        controller.on("flightDataLoaded", function (e, result) {
+//            equal(result[0].id, '123');
+//            equal(result[1].id, '234');
+//        });
+//        controller.dosomething();
+//    });
 
-});
+//});
 
-test("export method should only influence the module that has dependency the export module", function () {
-    expect(3);
+//test("export method should only influence the module that has dependency the export module", function () {
+//    expect(3);
 
-    kino.module("IService", {
-        myAPI: function () {
-        }
-    });
+//    kino.module("IService", {
+//        myAPI: function () {
+//        }
+//    });
 
-    kino.module("ModuleA", function () {
-        ok(true);
-    });
+//    kino.module("ModuleA", function () {
+//        ok(true);
+//    });
 
-    kino.module("ModuleB", ["IService"], function () {
-        ok(true);
-    });
+//    kino.module("ModuleB", ["IService"], function () {
+//        ok(true);
+//    });
 
-    kino.export("IService", {
-        myAPI: function () {
-            return "hello my api";
-        }
-    });
+//    kino.export("IService", {
+//        myAPI: function () {
+//            return "hello my api";
+//        }
+//    });
 
-    kino.use(["ModuleA", "ModuleB"], function (a, b) {
-    });
+//    kino.use(["ModuleA", "ModuleB"], function (a, b) {
+//    });
 
-});
+//});
 
 
